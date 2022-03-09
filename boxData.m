02dc6c745b3935020737aa1583b912621d3f4521
devnek <dev.vap03@gmail.com>
2022-03-10 01:49:38 +0530

    Update boxData.m

--- boxData.m
+++ boxData.m
@@ -9,7 +9,7 @@ classdef boxData < handle
         info
         time_ranges
         dataPath
-        baselineBox = box(signal = {'x405A','x465A','x560B'}, event = {'PC0_','PC2_','PC4_','PC6_'});
+        baselineBox = box('signal',{'x405A','x465A','x560B'}, 'event',{'PC0_','PC2_','PC4_','PC6_'});
         box = box(signal = {'x405C','x465C','x560D'}, event = {'PC1_','PC3_','PC5_','PC7_'});
     end
 
